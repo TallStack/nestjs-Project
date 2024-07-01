5b04e408c110acf089331d69c500cee08d606ca3
Keketso Ramosedi <kekeramosedi@gmail.com>
2024-07-01 13:20:57 +0200

    Added api description and example value for Post api

--- src/policies/policies.controller.ts
+++ src/policies/policies.controller.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/common';
 
 import { PoliciesService } from './policies.service';
-import { ApiAcceptedResponse, ApiBadRequestResponse, ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiAcceptedResponse, ApiBadRequestResponse, ApiBody, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Policy } from './policy.model';
 
 @ApiTags('Policies')
@@ -19,6 +19,24 @@ export class PoliciesController {
 
   @ApiCreatedResponse({ description: 'Policy created successfully', type: Policy })
   @ApiBadRequestResponse({ description: 'Invalid input' })
+  @ApiOperation({ summary: 'Create a new policy' })
+  @ApiBody({
+    type: Policy,
+    description: "The Description for the Post Body to be sent to create a new Policy.",
+    examples: {
+        a: {
+            summary: "Empty Body",
+            description: "When an empty body is used",
+            value: {} as Policy
+        },
+        b: {
+            summary: "Policy Body",
+            description: "When a Policy body is used",
+            value: Policy 
+        }
+    }
+})
+// @ApiBody({ type: Policy, description: 'Policy object' })
   @Post()
   addPolicy(
     @Body('title') policyTitle: string,
@@ -34,6 +52,7 @@ export class PoliciesController {
   }
 
   @ApiOkResponse({ description: 'Policies fetched successfully', type: Policy, isArray: true})
+  @ApiOperation({ summary: 'Get all policies' })
   @Get()
   getAllPolicies() {
     return this.policiesService.getPolicies();
@@ -41,12 +60,14 @@ export class PoliciesController {
 
 
   @ApiOkResponse({ description: 'Policy fetched successfully', type: Policy})
+  @ApiOperation({ summary: 'Get a single policy' })
   @Get(':id')
   getPolicy(@Param('id') policyId: string) {
     return this.policiesService.getSinglePolicy(policyId);
   }
 
   @ApiAcceptedResponse({ description: 'Policy updated successfully', type: Policy})
+  @ApiOperation({ summary: 'Update a policy' })
   @Patch(':id')
   updatePolicy(
     @Param('id') policyId: string,
@@ -58,7 +79,8 @@ export class PoliciesController {
     return null;
   }
 
-  @ApiAcceptedResponse({ description: 'Policy deleted successfully' })  
+  @ApiAcceptedResponse({ description: 'Policy deleted successfully' }) 
+  @ApiOperation({ summary: 'Delete a policy' })
   @Delete(':id')
   removePolicy(@Param('id') policyId: string) {
       this.policiesService.deletePolicy(policyId);
